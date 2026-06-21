0d0712ca573f605eded542c8da7050349dc0240c
k7gi <gbetteley2023@gmail.com>
2026-06-21 04:03:50 +0100

    Add new GamePlaceID for sbpb loader

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,6 @@
 local GamePlaceIDs = {
     [108234811088256] = "https://api.luarmor.net/files/v4/loaders/cbc074aac09aaf7212109ac9259b8bba.lua"; -- sbuc
+    [125348235306113] = "https://api.luarmor.net/files/v4/loaders/14c78b803603ae6a9d3536cb43f891a5.lua"; -- sbpb
 }
 
 if not GamePlaceIDs[game.PlaceId] then
